09a4b6c45243b660f045c937a7068e87e9d9c20d
NaveeraRajChuhan <nnfstars@gmail.com>
2025-03-21 20:50:59 +0500

    city

---
+++ app.js
@@ -0,0 +1,39 @@
+let city = prompt("enter your city")
+let city1 = karachi
+let city2 = india
+let city3 = canada
+let city4 = tokyo
+let city5 = korea
+let city6 = turkey
+let city7 = paris
+let city8 = nepal
+let city9 = indonasia
+let city10 = Lumineaux
+let city11 = Zephyropolis
+let city12 = Aetheria
+let city13 = Solanthea
+let city14 = Arvindale
+let city15 = Astridell
+let city16 = Serenium
+let city17 = siberia 
+// let city = 
+// let city = 
+// let city = 
+// let city = 
+// let city = 
+// let city = 
+// let city = 
+// let city = 
+// let city = 
+// let city = 
+// let city = 
+// let city = 
+
+
+
+if(Weather == 100){
+    
+}
+
+
+
